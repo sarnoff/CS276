e1544d8e506973ce1567d654b14b7a0683d6a377
sarnoff <sarnoff@stanford.edu>
2011-04-14 22:41:09 -0700

    program runs through the scorer - now to fill in the meat...

--- pe1/src/cs276/pe1/spell/KGramSpellingCorrector.java
+++ pe1/src/cs276/pe1/spell/KGramSpellingCorrector.java
@@ -2,6 +2,7 @@ package cs276.pe1.spell;
 
 import java.io.File;
 import java.util.List;
+import java.util.ArrayList;
 
 import cs276.util.IOUtils;
 import cs276.util.StringUtils;
@@ -9,7 +10,8 @@ import cs276.util.StringUtils;
 public class KGramSpellingCorrector implements SpellingCorrector {
 	/** Initializes spelling corrector by indexing kgrams in words from a file */
 	public KGramSpellingCorrector() {
-        File path = new File("/afs/ir/class/cs276/pe1-2011/big.txt.gz");
+        //File path = new File("/afs/ir/class/cs276/pe1-2011/big.txt.gz");
+        File path = new File("datasources/big.txt.gz");
         for (String line : IOUtils.readLines(IOUtils.openFile(path))) {
             for (String word : StringUtils.tokenize(line)) {
             	// TODO do kgram indexing
@@ -19,6 +21,6 @@ public class KGramSpellingCorrector implements SpellingCorrector {
 
 	public List<String> corrections(String word) {
 		// TODO fill in method here
-		return null;
+		return new ArrayList<String>();
 	}
 }
