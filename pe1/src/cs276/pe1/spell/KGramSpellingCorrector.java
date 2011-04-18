14ed90c8c7de7484296cad9e32de4e5a136276d2
sarnoff <sarnoff@stanford.edu>
2011-04-18 00:03:36 -0700

    factored out jaccard scoring and actually get the ratio (as opposed to just the count).

--- pe1/src/cs276/pe1/spell/KGramSpellingCorrector.java
+++ pe1/src/cs276/pe1/spell/KGramSpellingCorrector.java
@@ -13,7 +13,7 @@ import cs276.util.StringUtils;
 import cs276.util.Counter;
 
 public class KGramSpellingCorrector implements SpellingCorrector {
-    protected static int K = 2; //start with bigrams, then extend out
+    protected static int K = 4; //start with bigrams, then extend out
     protected static int SE = K-1;//3;//defines extra kgrams - default to k-1 for best performance?
     protected static int WL = 10;//returned Word List size
 	/** Initializes spelling corrector by indexing kgrams in words from a file */
@@ -57,16 +57,19 @@ public class KGramSpellingCorrector implements SpellingCorrector {
         set.add(value);
         kgram.put(key,set);
     }
-
-	public List<String> corrections(String word) {
-		Counter<String> wordCounts = new Counter<String>();
+    
+    protected Counter<String> jaccardScore(String word)
+    {
+        Counter<String> wordCounts = new Counter<String>();
         
         String extraKGram = "";
         for(int i = 0; i < SE; i++)
             extraKGram += "$";
-        
+            
         String key = extraKGram+word+extraKGram;
         Set<String> set;
+        
+        //if the word is smaller than the key size
         if(key.length() <= K)
         {
             if(kgram.containsKey(key))
@@ -75,16 +78,23 @@ public class KGramSpellingCorrector implements SpellingCorrector {
                 for(String w : set)
                     wordCounts.incrementCount(w);
             }
+            return wordCounts;
         }
-        for(int i = 0;i<(key.length()-K+1);i++)
+        //otherwise iterate through the kgram and increment the count
+        double numKeys = key.length()-K+1;
+        for(int i = 0;i<numKeys;i++)
         {
             if(kgram.containsKey(key.substring(i,i+K)))
             {
                 set = kgram.get(key.substring(i,i+K));
                 for(String w : set)
-                    wordCounts.incrementCount(w);
+                    wordCounts.incrementCount(w,1.0/numKeys);
             }
         }
-		return wordCounts.topK(WL);
+        return wordCounts;
+    }
+
+	public List<String> corrections(String word) {
+		return jaccardScore(word).topK(WL);
 	}
 }
