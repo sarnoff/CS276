812ad3cf0d98b66a672c5996fc9ff50058f9fa0e
Amanda <xxstealthestarsxx@gmail.com>
2011-04-20 22:24:43 -0700

    Part 3

--- pe1/src/cs276/pe1/lucene/IMDBSpelling.java
+++ pe1/src/cs276/pe1/lucene/IMDBSpelling.java
@@ -1,6 +1,9 @@
 package cs276.pe1.lucene;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -17,34 +20,37 @@ import org.apache.lucene.search.spell.LuceneDictionary;
 import org.apache.lucene.search.spell.PlainTextDictionary;
 import org.apache.lucene.search.spell.SpellChecker;
 
+import cs276.pe1.spell.KGramSpellingCorrector;
+import cs276.pe1.spell.KGramWithEditDistanceSpellingCorrector;
+
 @SuppressWarnings("deprecation")
-public class IMDBSpelling {
-	public static final String words = "\\\\afs\\ir\\class\\cs276\\pe1-2011\\spelltest.txt";
-	
+public class IMDBSpelling {	
 	public static void main(String[] args) throws Exception {
+		//Read search query
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+		String search = reader.readLine();
+		
+		//Spelling correct query
+		KGramWithEditDistanceSpellingCorrector k = new KGramWithEditDistanceSpellingCorrector();
+		List<String> suggest = k.corrections(search);
+		for(String s : suggest) search += " " + s;
+		
+		//Run Lucene search
 		Directory index = FSDirectory.getDirectory(new File(new File(System.getProperty("user.home")),"cs276-index"));
 		IndexSearcher searcher = new IndexSearcher(index);
-		Analyzer analyzer = new StandardAnalyzer();
+		Analyzer analyzer = new StandardAnalyzer();		
+		QueryParser parser = new QueryParser("title",analyzer);
+		Query query = parser.parse(search);
+		Hits hits = searcher.search(query);
 		
-		Directory spellIndex = FSDirectory.getDirectory(new File(new File(System.getProperty("user.home")),"cs276-spell"));
-		SpellChecker spell = new SpellChecker(spellIndex);
-		IndexReader reader = IndexReader.open(index);
-		Dictionary dictionary = new LuceneDictionary(reader, "title");
-		spell.indexDictionary(dictionary);
-		
-		String[] suggest = spell.suggestSimilar("Trmmy", 10);
-		for(String s : suggest) System.out.println(s);
-		System.out.println("done");
-		
-//		QueryParser parser = new QueryParser("title",analyzer);
-//		Query query = parser.parse("Trmmy");
-//		Hits hits = searcher.search(query);
-//		System.out.println("Results for Timmy: "+hits.length());
-//		int num = Math.min(Integer.MAX_VALUE, hits.length());
-//		for(int i = 0; i < num; i++) {
-//			Document hit = hits.doc(i);
-//			System.out.println("  "+hit.get("title"));
-//		}
+		//Display results
+		System.out.println("Results: "+hits.length());
+		int num = Math.min(20, hits.length());
+		for(int i = 0; i < num; i++) {
+			Document hit = hits.doc(i);
+			System.out.println("  "+hit.get("title"));
+		}
 		index.close();
 		searcher.close();
 	}
